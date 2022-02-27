60328956fe6761ac2cb5f40d743665f58e8c8d4f
Tanvir Tahmid <tanv.mhn@gmail.com>
2022-02-28 00:30:04 +0600

    add timestamp to user schema

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ class Database {
     }
     
     connect() {
-        mongoose.connect("mongodb+srv://tanvir:H3PqnxIppMkfCJGF@raven-social-media-clus.1dtzn.mongodb.net/ravendb?retryWrites=true&w=majority")
+        mongoose.connect("mongodb://tanvir:H3PqnxIppMkfCJGF@raven-social-media-clus-shard-00-00.1dtzn.mongodb.net:27017,raven-social-media-clus-shard-00-01.1dtzn.mongodb.net:27017,raven-social-media-clus-shard-00-02.1dtzn.mongodb.net:27017/ravendb?ssl=true&replicaSet=atlas-9bvpn8-shard-0&authSource=admin&retryWrites=true&w=majority")
         .then(() => {
             console.log("database connection successful");
         })
