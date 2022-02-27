dc13be8fb71fd226925bd80e47bed4d5e9a0563b
Tanvir Tahmid <tanv.mhn@gmail.com>
2022-02-28 00:41:52 +0600

    fix page typo, add sessions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require('./middleware');
 const path = require('path');
 const bodyParser = require("body-parser");
 const mongoose = require("./database");
+const session = require('express-session');
 
 const server = app.listen(port, () => console.log("listening on port " + port));
 
@@ -13,6 +14,12 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")))
+
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: false
+}))
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
