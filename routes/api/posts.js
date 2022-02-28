eda5844ba7fd73e09caefbe2a256a98f742db4e1
Tanvir Tahmid <tanv.mhn@gmail.com>
2022-02-28 16:11:03 +0600

    add and style create posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
 const User = require('../../schemas/UserSchema');
+const Post = require('../../schemas/PostSchema');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -11,7 +12,27 @@ router.get("/", (req, res, next) => {
 })
 
 router.post("/", async (req, res, next) => {
-    res.status(200).send("it worked");
+
+    if (!req.body.content) {
+        console.log("Content param not sent with request");
+        return res.sendStatus(400);
+    }
+
+    var postData = {
+        content: req.body.content,
+        postedBy: req.session.user
+    }
+
+    Post.create(postData)
+    .then(async newPost => {
+        newPost = await User.populate(newPost, { path: "postedBy" })
+
+        res.status(201).send(newPost);
+    })
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
 })
 
 module.exports = router;
